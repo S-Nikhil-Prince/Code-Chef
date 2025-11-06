72f28c5ddf9dfb0295d3353e1998e6d3d5b52d29
Nikhil Prince Sodadasi <166209449+S-Nikhil-Prince@users.noreply.github.com>
2025-11-06 21:26:32 +0530

    Commit 5: Add for loop

--- test.py
+++ test.py
@@ -3,3 +3,5 @@ print('Random commit 1 - testing functionality')
 x = 42  # Random variable assignment
 # This is a random comment for commit 3
 y = ['apple', 'banana', 'cherry']
+for item in y:
+    print(item)
